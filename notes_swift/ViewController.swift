37ffa4df5007f2b83b2f510b9706a9a8e5ac0160
Дмитрий  Маношин <dmitry@MacBook-Air-dmitry.local>
2021-02-16 02:18:14 +1000

    init

---
+++ notes_swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  notes_swift
+//
+//  Created by Дмитрий  Маношин  on 16.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
