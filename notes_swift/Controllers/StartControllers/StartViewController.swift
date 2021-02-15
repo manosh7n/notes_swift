77f37f3d9de0a2ee99eb4903dbbdcd8ea3316168
Дмитрий  Маношин <dmitry@MacBook-Air-dmitry.local>
2021-02-16 03:48:07 +1000

    added login, signup, start screens / login layout

---
+++ notes_swift/Controllers/StartControllers/StartViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  notes_swift
+//
+//  Created by Дмитрий  Маношин  on 16.02.2021.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+    }
+    
+    @IBAction func toLogin() {
+        self.navigationController?.pushViewController(getViewControllerWithID(identifier: "LoginController"), animated: true)
+    }
+    
+    @IBAction func toSignUp() {
+        self.navigationController?.pushViewController(getViewControllerWithID(identifier: "SignUpController"), animated: true)
+    }
+    
+    func getViewControllerWithID(identifier: String) -> UIViewController {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        return storyboard.instantiateViewController(withIdentifier: identifier)
+    }
+
+}
+
